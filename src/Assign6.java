a5c5418ab49f9c572a4c40ebac29ee84d1e385ba
Steve Tate <srtate@uncg.edu>
2016-11-17 14:12:37 -0500

    Initial code and data files for Assignment 6

---
+++ src/Assign6.java
@@ -0,0 +1,16 @@
+/**
+ * Code for Assignment 5 in UNCG's CSC 330 (Fall 2016).
+ *
+ * @author Steve Tate
+ */
+
+public class Assign6 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Graph g = Graph.readGraph("a6graph1.in");
+        g.printStronglyConnected();
+    }
+    
+}
